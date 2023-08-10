5d0a4f058dfb57282812590bed50ac9609d2cae1
vinicio Brejinski <viniciosdb@gmail.com>
2023-08-10 19:35:33 -0300

    Demo - transacao

--- src/apllication/Program.java
+++ src/apllication/Program.java
@@ -1,34 +1,43 @@
 package apllication;
 
 import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import db.DB;
-import db.DbIntegrityException;
+import db.DbException;
 
 public class Program {
 
     public static void main(String[] args) {
         Connection conn = null;
-        PreparedStatement st = null;
+        Statement st = null;
 
         try {
             conn = DB.getConnection();
-            st = conn.prepareStatement(
-                    "DELETE FROM department "
-                    + "WHERE "
-                    + "Id = ?"
-            );
+            conn.setAutoCommit(false);
+            st = conn.createStatement();
+            int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+            int x = 1;
 
-            st.setInt(1, 2);
+            if (x < 2) {
+                throw new SQLException("Fake Error");
+            }
 
-            int rowsAffected = st.executeUpdate();
+            int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
 
-            System.out.println("Done! rows affected " + rowsAffected);
+            conn.commit();
+
+            System.out.println("rows1" + rows1);
+            System.out.println("rows2" + rows2);
+
 
         } catch (SQLException e) {
-            throw new DbIntegrityException(e.getMessage());
+            try {
+                conn.rollback();
+                throw new DbException("Erro na transacao");
+            } catch (SQLException ex) {
+                throw new DbException(ex.getMessage());
+            }
+
         } finally {
             DB.closeStatement(st);
             DB.closeConnection();
